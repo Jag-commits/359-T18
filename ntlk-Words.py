c884a5f414632e0ca17b597864d57e096abbc60a
Jag-commits <Jagpreetdhaliwal06@gmail.com>
2026-01-09 10:19:52 -0800

    Added wordshuffler -Jagpreet

--- ntlk-Words.py
+++ ntlk-Words.py
@@ -1,6 +1,6 @@
 #Make sure to pip install nltk into the venv
 import nltk
-
+import random
 
 #Python doesn't enforce explicit return types, but it's good practice to type hint to avoid errors
 def retrieveWords() -> list:
@@ -23,3 +23,16 @@ def createWords():
     nltk.data.path.append("./Words")
     print("Words Created")
     
+#Just a function to repeatedly call based on the length of the desired list
+def randomWordsList(length : int, wordlist: list[str]) -> list[str]:
+    #I doubt we'd actually reach this point, but edge case gets handled
+    #I initially made a for-loop, but this is so much cleaner
+    length = min(length,len(wordlist))
+
+    randomlist =random.sample(wordlist,length)
+    #Super and super are etymologically the same, it's just that Super is capitalized. I don't see how Super > super just on the basis of capitalization if the words are exactly the same
+    #Therefore, I'm making all the words lowercase to ensure the sorting algorithm won't define Super>super instead of Super = super
+    for x in range(0,length):
+        randomlist[x] = randomlist[x].lower()
+
+    return randomlist
